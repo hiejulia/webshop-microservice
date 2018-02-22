4d7f9333326f6c7b695351fe022186bf76ba4806
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-02-22 22:01:16 +0200

    update email service with rabbitmq

--- email-service/src/main/java/project/webshop/WebshopApplication.java
+++ email-service/src/main/java/project/webshop/WebshopApplication.java
@@ -1,10 +1,61 @@
 package project.webshop;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import project.webshop.messaging.Receiver;
 
 @SpringBootApplication
 public class WebshopApplication {
+	// queue name
+    final static String queueName = "mail-queue";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("email-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    Receiver receiver() {
+        return new Receiver();
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebshopApplication.class, args);
---
+++ email-service/src/main/java/project/webshop/messaging/Receiver.java
@@ -0,0 +1,22 @@
+package project.webshop.messaging;
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.CountDownLatch;
+import org.apache.commons.codec.binary.Base64;
+import project.webshop.domain.Mail;
+
+public class Receiver {
+
+    public void receiveMessage(byte[]  message) throws UnsupportedEncodingException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Mail mail = new Mail();
+        String strMessage=new String(message, "UTF-8");
+        System.out.println("THIS IS THE strMESSAGE"+strMessage);
+        byte[] valueDecoded= Base64.decodeBase64(strMessage);
+        System.out.println("Decoded value is " + new String(valueDecoded));
+        mail.sendEmail(new String(valueDecoded));
+        latch.countDown();
+
+    }
+
+
+}
