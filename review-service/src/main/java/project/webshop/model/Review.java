e08a04c895a79649abed5191dfc6a26418a38574
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-02-24 01:13:48 +0200

    update review domain

--- review-service/src/main/java/project/webshop/model/Review.java
+++ review-service/src/main/java/project/webshop/model/Review.java
@@ -14,9 +14,36 @@ public class Review {
 
     // REVIEW: productId, rating, customerId
 
+    private UUID reviewId;
     private UUID productId;
     private double rating;
+    private String content;
+    private String subject;
     private UUID customerId;
+
+    public UUID getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(UUID reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 
     public UUID getCustomerId() {
         return customerId;
