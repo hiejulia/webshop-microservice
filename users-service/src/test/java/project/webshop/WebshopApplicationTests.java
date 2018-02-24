9b45270c9560acb3e0cd482b49902f673248fe90
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-02-24 11:45:32 +0200

    update unit and integration test for security

--- users-service/src/test/java/project/webshop/WebshopApplicationTests.java
+++ users-service/src/test/java/project/webshop/WebshopApplicationTests.java
@@ -2,11 +2,13 @@ package project.webshop;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebMvcTest(secure = false)// disable security
 public class WebshopApplicationTests {
 
 	@Test
--- users-service/src/test/java/project/webshop/integration/UserSecurityRestTest.java
+++ users-service/src/test/java/project/webshop/integration/UserSecurityRestTest.java
@@ -1,10 +1,52 @@
 package project.webshop.integration;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.nio.charset.Charset;
+import java.util.Base64;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserSecurityRestTest {
+
+    private TestRestTemplate template = new TestRestTemplate();
+
+    // header with basic auth
+    HttpHeaders headers = createHeaders("user-name", "user-password");
+
+    HttpHeaders createHeaders(String username, String password) {
+        return new HttpHeaders() {
+            {
+                String auth = username + ":" + password;
+                byte[] encodedAuth = Base64.getEncoder().encode
+                        (auth.getBytes(Charset.forName("US-ASCII")));
+                String authHeader = "Basic " + new String(encodedAuth);
+                set("Authorization", authHeader);
+            }
+        };
+    }
+
+    @Test
+    public void retrieveUsers() throws Exception {
+        // mock expected json returned
+        String expected = "["
+                + "{id:1,user:Julia}" + ","
+                + "{id:2,user:Hien}" + "]";
+
+        ResponseEntity<String> response = template.exchange(
+                createUrl("/users/Jack/todos"), HttpMethod.GET,
+                new HttpEntity<String>(null, headers),
+                String.class);
+        JSONAssert.assertEquals(expected, response.getBody(), false);
+    }
+
 }
